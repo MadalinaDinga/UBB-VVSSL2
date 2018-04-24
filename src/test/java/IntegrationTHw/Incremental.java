bedde82cdce4a01b4ac28efea53637da2b366aca
MadalinaDinga <madadinga@gmail.com>
2018-04-25 00:09:48 +0300

    + incremental

---
+++ src/test/java/IntegrationTHw/Incremental.java
@@ -0,0 +1,11 @@
+package IntegrationTHw;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Incremental {
+    @Test
+    public void testBigBang() {
+        Assert.assertTrue(true);
+    }
+}
